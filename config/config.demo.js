5800daaa56c0bb05fd8c1b1c197c72f649ac477d
Nicola <nicola.delazzari@gmail.com>
2013-07-14 11:42:35 +0200

    url guide

--- config/config.demo.js
+++ config/config.demo.js
@@ -1,7 +1,9 @@
 var configFoursquare = {
-  	clientId: "",
-   	clientSecret: "",
-    oauth_token:"",
-    // code: "",
-	redirectUrl : ""
+  	clientId: "your clientId",
+   	clientSecret: "your clientSecret",
+    oauth_token:"your oauth_token",
+    userId:"your userId",
+	redirectUrl : "your redirectUrl"
 };
+
+// https://developer.foursquare.com/overview/auth
